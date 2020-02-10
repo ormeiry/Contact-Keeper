f647de96c52bdb23759a74a2f514d29fa1ac805c
ormeiry <ormeiry2411@gmail.com>
2020-02-10 15:34:44 +0200

    Uploading to GitHub

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack react app for keeping contacts
+      </p>
+      <p className='bg-dark p'>
+        <strong>Vesrion:</strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default Home;
